4efb479bfebdf021d0392037acf66da1580052eb
Siddhartha Roy <sid86harth@gmail.com>
2021-08-23 00:03:58 +0530

    token_hide

--- main.py
+++ main.py
@@ -12,10 +12,11 @@ async def on_ready():
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Over this Server"))
     print("Dev Bot is active")
 
+# main function
 @client.event
 async def on_member_join(member):
-    guild = client.get_guild(877069857852645406)
-    channel = guild.get_channel(878581314700312586)
+    guild = client.get_guild('server_id_here')
+    channel = guild.get_channel('channel_id_here')
     await channel.send(f"Welcome to the developer's server  {member.mention} ! :partying_face:")
     await member.send(f"Welsome to the {guild.name} server, {member.name}! :partying_face:")
 
@@ -24,5 +25,5 @@ async def on_message(message):
     if message.content == 'hi':
         await message.channel.send(f"Hi!")
 
-token = 'ODc4NTk1Mjg3ODg1NzA1MjI2.YSDdoA.xB2JeOPK1hVA8IfIeL9XLXU3MPI'
+token = 'app_token'
 client.run(token)
